48b0058000e58d3e8b47eeef99e0f2659d7166dc
Ryan Kwan <klhong124@gmail.com>
2023-09-04 23:24:03 +0100

    ui quick fix

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,10 @@ const { floor } = storeToRefs(store);
 
 <template>
   <div class="glass m-12 p-8 ">
-    <div class="font-primary text-5xl uppercase text-black">
+    <div class="font-primary text-3xl uppercase text-black">
       Fizz Buzz Elevator
     </div>
-    <p class="mt-4 ">created by <a href="https://ryankwan.vercel.app/" target="_blank" class="underline">Ryan Kwan</a> | <a href="https://github.com/klhong124/FizzBuzz" target="_blank" class="underline">GitHub Source Code</a></p>
+    <p class="mt-3">created by <a href="https://ryankwan.vercel.app/" target="_blank" class="underline">Ryan Kwan</a> | <a href="https://github.com/klhong124/FizzBuzz" target="_blank" class="underline">GitHub Source Code</a></p>
   </div>
 </template>
 
